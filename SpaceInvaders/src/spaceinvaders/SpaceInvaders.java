aa337092e887f9e12cb2153e343469f53eaa01bf
LuanGC <luan.gehm.cappelari@gmail.com>
2018-05-17 21:38:52 -0300

    Em andamento 02

--- SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
+++ SpaceInvaders/src/spaceinvaders/SpaceInvaders.java
@@ -19,36 +19,40 @@ public class SpaceInvaders {
         //Local que fica os inimigos
         Inimigo[] meusmonstros = new Inimigo[12];
         //Local que fica os tiros
-        Tiro[] meustiros = new Tiro[100];
+        Tiro[] meustiros = new Tiro[120];
         //Quantidade de tiros na tela
         int quantidadeTiros = 0;
         int quantidadeMonst = 12;
         int posTiro = 76;
 
         //Crio os monstros na tela
-        meusmonstros[0] = new Inimigo(2, 5, 2);
-        meusmonstros[1] = new Inimigo(15, 5, 2);
-        meusmonstros[2] = new Inimigo(28, 5, 2);
-        meusmonstros[3] = new Inimigo(41, 5, 2);
-        meusmonstros[4] = new Inimigo(54, 5, 2);
-        meusmonstros[5] = new Inimigo(67, 5, 2);
-        meusmonstros[6] = new Inimigo(2, 15, 2);
-        meusmonstros[7] = new Inimigo(15, 15, 2);
-        meusmonstros[8] = new Inimigo(28, 15, 2);
-        meusmonstros[9] = new Inimigo(41, 15, 2);
-        meusmonstros[10] = new Inimigo(54, 15, 2);
-        meusmonstros[11] = new Inimigo(67, 15, 2);
+        meusmonstros[0] = new Inimigo(2, 5, 1);
+        meusmonstros[1] = new Inimigo(15, 5, 1);
+        meusmonstros[2] = new Inimigo(28, 5, 1);
+        meusmonstros[3] = new Inimigo(41, 5, 1);
+        meusmonstros[4] = new Inimigo(54, 5, 1);
+        meusmonstros[5] = new Inimigo(67, 5, 1);
+        meusmonstros[6] = new Inimigo(2, 15, 1);
+        meusmonstros[7] = new Inimigo(15, 15, 1);
+        meusmonstros[8] = new Inimigo(28, 15, 1);
+        meusmonstros[9] = new Inimigo(41, 15, 1);
+        meusmonstros[10] = new Inimigo(54, 15, 1);
+        meusmonstros[11] = new Inimigo(67, 15, 1);
         //Inicia o jogo
         Space.init();
 
-        for (int i = 0; i < 55; i++) {
+        for (int i = 0; i < 110; i++) {
             //Move os bonecos
             for (int j = 0; j < 12; j++) {
-                meusmonstros[j].setY(meusmonstros[j].getY() + 1);
-                 if(meusmonstros[j].getY()==70){
+
+                if(i % 2 == 0){
+                    meusmonstros[j].setY(meusmonstros[j].getY() + 1);
+            }
+                if(meusmonstros[j].getY()==70){
                     Space.gameOver();
                   
                 }
+                       
             }
         
 
@@ -56,8 +60,27 @@ public class SpaceInvaders {
             
                 for (int t = 0; t < quantidadeTiros; t++) {
                     meustiros[t].y = meustiros[t].y-1;
-                }
-            
+                  for (int j = 0; j < 12; j++) {
+                    if (meustiros[t].y == meusmonstros[j].y + 6) {
+                            if (meustiros[t].x >= meusmonstros[j].x
+                                    || meustiros[t].x == (meusmonstros[j].x - 1)
+                                    || meustiros[t].x == (meusmonstros[j].x + 1)
+                                    || meustiros[t].x == (meusmonstros[j].x + 2)
+                                    || meustiros[t].x == (meusmonstros[j].x + 3)
+                                    || meustiros[t].x == (meusmonstros[j].x + 4)
+                                    || meustiros[t].x == (meusmonstros[j].x + 5)
+                                    || meustiros[t].x == (meusmonstros[j].x + 6)
+                                    || meustiros[t].x == (meusmonstros[j].x + 7)
+                                    || meustiros[t].x == (meusmonstros[j].x + 8)
+                                    || meustiros[t].x == (meusmonstros[j].x + 9)
+                                    || meustiros[t].x == (meusmonstros[j].x + 10)) {
+                        meusmonstros[j].setX(-50); 
+                        meusmonstros[j].setY(200);
+                        meustiros[t].y = 200;
+                    }
+                    }
+                  }
+              }
 
                 System.out.println(Space.getPlatX());
                 if (Space.apertouDireita()) {
@@ -87,3 +110,4 @@ public class SpaceInvaders {
 
         }
 }
+
